0cbd7659d68291906a5f5c9a90e63da836544990
Aviran Katz <aviran@soficoop.com>
2019-10-07 09:55:00 +0300

    Plan.vue refactored

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="pa-md-12">
-    <v-layout fill-height wrap align-content-start>
-      <v-flex xs12 pb-3>
+    <v-row>
+      <v-col>
         <h3 class="headline primary--text font-weight-black">
           <span v-if="plan.number" tabindex="0">{{ plan.number }}</span>
           <span v-if="plan.number && plan.name"> • </span>
@@ -18,11 +18,13 @@
           <v-icon right color="primary">mdi-update</v-icon>
           <span tabindex="0">{{ plan.status }}</span>
         </h5>
-      </v-flex>
-      <v-layout xs12 wrap row v-if="plan.targets || plan.sections">
-        <v-flex xs12 md8 pa-3>
-          <v-flex pb-3 v-if="plan.targets">
-            <v-card flat class="pa-4">
+      </v-col>
+    </v-row>
+    <v-row v-if="plan.targets || plan.sections">
+      <v-col cols="12" md="8">
+        <v-row no-gutters>
+          <v-col>
+            <v-card v-if="plan.targets" flat class="pa-4">
               <h4 class="title primary--text" tabindex="0">מטרות</h4>
               <p
                 class="whitespace-preline"
@@ -30,9 +32,11 @@
                 v-html="plan.targets"
               ></p>
             </v-card>
-          </v-flex>
-          <v-flex pt-3 v-if="plan.sections">
-            <v-card flat class="pa-4">
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card v-if="plan.sections" flat class="pa-4">
               <h4 class="title primary--text" tabindex="0">
                 עיקרי ה{{ planTypeFirstWord }}
               </h4>
@@ -42,50 +46,60 @@
                 v-html="plan.sections"
               ></p>
             </v-card>
-          </v-flex>
-        </v-flex>
-        <v-flex xs12 md4 pa-3>
-          <v-card flat class="pa-4">
-            <h4 class="title primary--text" tabindex="0">נתונים</h4>
-            <Map
-              class="my-3"
-              :query="planLocationQuery"
-              v-if="planLocationQuery"
-            />
-            <v-layout
-              py-1
-              wrap
-              v-for="infoItem in planInformation"
-              :key="infoItem.key"
-            >
-              <v-flex xs4 class="font-weight-semibold">
-                <span tabindex="0">{{ infoItem.key }}</span>
-              </v-flex>
-              <v-flex offset-xs1 xs7>
-                <span tabindex="0">{{ infoItem.value }}</span>
-              </v-flex>
-            </v-layout>
-          </v-card>
-        </v-flex>
-      </v-layout>
-      <h2 v-else class="display-1 accent--text my-3">
-        אין מידע זמין על ה{{ planTypeFirstWord }}
-      </h2>
-      <v-flex xs12 py-3 v-if="plan.attachedFiles.length">
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-card flat class="pa-4">
+          <h4 class="title primary--text" tabindex="0">נתונים</h4>
+          <Map
+            class="my-3"
+            :query="planLocationQuery"
+            v-if="planLocationQuery"
+          />
+          <v-row
+            v-for="infoItem in planInformation"
+            :key="infoItem.key"
+            no-gutters
+            class="py-1"
+          >
+            <v-col cols="4" class="font-weight-semibold">
+              <span tabindex="0">{{ infoItem.key }}</span>
+            </v-col>
+            <v-col cols="7" offset="1">
+              <span tabindex="0">{{ infoItem.value }}</span>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col>
+        <h2 class="display-1 accent--text my-3">
+          אין מידע זמין על ה{{ planTypeFirstWord }}
+        </h2>
+      </v-col>
+    </v-row>
+    <v-row v-if="plan.attachedFiles.length">
+      <v-col>
         <h4 class="title primary--text" tabindex="0">מסמכים רלוונטים</h4>
         <FileCards class="py-1" :files="plan.attachedFiles" />
-      </v-flex>
-      <v-flex xs12 py-3 v-if="planMeetings && planMeetings.length">
+      </v-col>
+    </v-row>
+    <v-row v-if="planMeetings && planMeetings.length">
+      <v-col>
         <h4 class="title primary--text" tabindex="0">
           היסטוריית הדיונים ב{{ planTypeFirstWord }}
         </h4>
         <MeetingCards :meetings="planMeetings"></MeetingCards>
-      </v-flex>
-      <v-flex xs12 py-3>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <h4 class="title primary--text" tabindex="0">התייחסויות</h4>
         <Comments></Comments>
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
