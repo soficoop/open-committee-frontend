07d42edac40552474335725b4465269c00384872
Ariella Wills <Ariellaw@gmail.com>
2019-11-06 09:46:54 +0200

    the registration tab is now active by default

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -306,7 +306,7 @@ export default class Login extends Vue {
     email: "",
     mailSent: ""
   };
-  tab = null;
+  tab = 1;
   authenticationFailed = false;
   userIsNotConfirmedMsg = "";
   dialog = false;
